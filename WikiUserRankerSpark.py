7c400d1791c66a6ca37ae3c47a3de5f8a4f02c58
andrewpetersen <andrew.patrick.petersen@gmail.com>
2020-10-11 17:29:15 -0700

    Added timings with perf_counter

--- WikiUserRankerSpark.py
+++ WikiUserRankerSpark.py
@@ -27,26 +27,35 @@ from pyspark.sql import functions as func
 from pyspark.sql import Window
 from pyspark.sql import SQLContext
 
-from ast import literal_eval
+from time import perf_counter
 
 #parameters to set
 region = 'us-west-1'
 bucket = 'petersen-insight-s3'
 key = 'supernova20200916.xml'
 #key = 'enwiki-20200401-pages-articles-multistream.xml'
+#key = 'trwiki-latest-pages-articles-multistream.xml'
 appName = "Petersen_XML_S3_Postgres_Testing"
-#master = "local" #add .master(master) after appName()?? #I think this is dealt with in the cmd line spark-submit call
 
 if __name__ == "__main__":
+    tTotal = perf_counter()
+    # Deal with input arguments, if any
     print("Length of sys.argv: ", len(sys.argv)) #Print number of input arguments
     print("sys.argv: ",sys.argv) #Print input arguments
+
+    # Setup Spark/SQL Contexts and Sessions
+    tSetup = perf_counter()
     sc = SparkContext()
     sc.setLogLevel("ERROR") #Hide unnecessary status information from standard output
     sc._jsc.hadoopConfiguration().set('fs.s3a.endpoint', f's3-{region}.amazonaws.com')
     spark = SparkSession(sc)
     sqlContext = SQLContext(sc)
+    tSetupEnd = perf_counter()
+
+    # Loading File Prep
+    tLoadFile = perf_counter()
     s3file = f's3a://{bucket}/{key}'
-    
+
 # Should specify schema for faster ingestion:
 #    schema = StructType([
 #        StructField('id', IntegerType(),False),
@@ -55,48 +64,87 @@ if __name__ == "__main__":
 #        StructField('timestamp', StringType(),False)
 #    ])
 
+    # Load file from S3
     df = spark.read.format("com.databricks.spark.xml") \
         .option("rowTag","revision").load(s3file)
     #    .option("rowTag","revision").load(s3file, schema=schema)  # switch to include predefined schema
+    tLoadFileEnd = perf_counter()
+ 
+
     df.show()
     print("df.dtypes: ",df.dtypes)
 
+
+    # Clean user information, add 2 columns: cleanContributor, isRegistered
+    tCleanUsers = perf_counter()
     df = df.withColumn("isRegistered", when(func.length(col("contributor").username)!=0,True).otherwise(False))
     df = df.withColumn("cleanContributor",when(col("isRegistered"),col("contributor").username).otherwise(col("contributor").ip))
+    tCleanUsersEnd = perf_counter()
     df.show()
+
     # Convert Contributor column from type Struct to type String
-#    print("df.dtypes: ",df.dtypes)
+    tStructString = perf_counter()
     df = df.withColumn("contributor", func.col("contributor").cast("String"))
+    tStructStringEnd = perf_counter()
     print("df.dtypes: ",df.dtypes)
 
     # using timestamp column, convert from string to timestamp, convert to epoch seconds, shift and subtract.
+    tTimestamp = perf_counter()
     df = df.withColumn("EpochTimestampStart",func.to_timestamp("timestamp").cast("long"))
     window = Window.partitionBy().orderBy("EpochTimestampStart")
     df = df.withColumn("EpochTimestampEnd", func.lag(df.EpochTimestampStart,-1).over(window))
     df = df.withColumn("LiveSeconds", func.when(func.isnull(df.EpochTimestampEnd - df.EpochTimestampStart), 0).otherwise(df.EpochTimestampEnd - df.EpochTimestampStart))
+    tTimestampEnd = perf_counter()
     df.show()
 
     # Score each contributor based on the average and total life of their revisions
+    tScoring = perf_counter()
     contributors = Window.partitionBy("contributor")
     scoreDf = df.withColumn("Score-Sum", func.sum("LiveSeconds").over(contributors)) \
               .withColumn("Score-Avg", func.avg("LiveSeconds").over(contributors)) \
               .withColumn("Score-Count", func.count("LiveSeconds").over(contributors)) \
               .select("cleanContributor","Score-Sum","Score-Avg","Score-Count","isRegistered").distinct().orderBy("Score-Sum","Score-Avg","Score-Count","cleanContributor",ascending=False)
     totalRows = scoreDf.count()
+    tScoringEnd = perf_counter()
     scoreDf.show(totalRows,truncate=False)
 
-    # Simplify Contributor Column Format
-    #literal_eval()
-
     # Write output to Postgresql database
+    tWriteDB = perf_counter()
     url = 'jdbc:postgresql://10.0.0.11:5442/cluster_output'
     postgresUser = os.environ['POSTGRES_USER']
     postgresPass = os.environ['POSTGRES_PASS']
     properties = {"user":postgresUser,"password":postgresPass,"driver":"org.postgresql.Driver"}
+    tWriteDBEnd = perf_counter()
     scoreDf.write.jdbc(url=url, table="ContributorScores",mode="overwrite",properties=properties)
 
     # Read from Postgresql database to confirm prior write
+    tReadDB = perf_counter()
     readSqlDf = spark.read.jdbc(url=url,table="ContributorScores",properties=properties) 
     readSqlDf.show(totalRows);
+    tReadDBEnd = perf_counter()
+
+    # Deal with timings
+    tTotalEnd = perf_counter()
+
+    TotalTime = tTotalEnd - tTotal
+    SetupTime = tSetupEnd - tSetup
+    LoadFileTime = tLoadFileEnd - tLoadFile
+    CleanUsersTime = tCleanUsersEnd - tCleanUsers
+    StructStringTime = tStructStringEnd - tStructString
+    TimestampTime = tTimestampEnd - tTimestamp
+    ScoringTime = tScoringEnd - tScoring
+    WriteDBTime = tWriteDBEnd - tWriteDB
+    ReadDBTime = tReadDBEnd - tReadDB
+
+    print("Input File: ",key)
+    print("TotalTime: ",TotalTime)
+    print("SetupTime: ",SetupTime)
+    print("LoadFileTime: ",LoadFileTime)
+    print("CleanUsersTime: ",CleanUsersTime)
+    print("StructStringTime: ",StructStringTime)
+    print("TimestampTime: ",TimestampTime)
+    print("ScoringTime: ",ScoringTime)
+    print("WriteDBTime: ",WriteDBTime)
+    print("ReadDBTime: ",ReadDBTime)
 
     spark.stop()
